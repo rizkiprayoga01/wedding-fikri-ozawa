1b1bf54642c1e87657cc976391fc79b84e45edaa
rizkiprayoga01 <bangikky22@gmail.com>
2022-06-01 20:54:35 +0700

    initial commit

---
+++ js/countdown.js
@@ -0,0 +1,25 @@
+
+var countDownDate = new Date("Jun 11, 2022 08:00:00").getTime();
+
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days + "<br><small>&nbsp Days &nbsp</small>";
+  document.getElementById("hours").innerHTML = hours + "<br><small>&nbsp Hours &nbsp</small> ";
+  document.getElementById("minutes").innerHTML = minutes + "<br><small>&nbsp Minutes &nbsp</small> ";
+  document.getElementById("seconds").innerHTML = seconds + "<br><small>&nbsp Seconds &nbsp</small> ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "PERNIKAHAN TELAH SELESAI";
+  }
+}, 1000);
